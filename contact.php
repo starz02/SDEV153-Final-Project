6e5a3dfd69fcc2d8a6fc76b11e1bf386b0c050f1
Caden Ruschmeier <156849131+starz02@users.noreply.github.com>
2024-03-06 21:13:18 -0500

    w

--- contact.php
+++ contact.php
@@ -2,11 +2,11 @@
 if (isset($_POST['email']) && !empty($_POST['email'])) {
 $subject = "New contact request: $_POST['subject']";
 $message = $_POST['body'];
-$headers = 'From: cruschmeier@ivytech.edu' . "\r\n" .
+$headers = 'From: info@example.com' . "\r\n" .
            'Reply-To: ' . $_POST['email']. "\r\n" .
            'X-Mailer: PHP/' . phpversion();
 
-mail('info@example.com', $subject, $message, $headers);
+mail('cruschmeier@ivytech.edu', $subject, $message, $headers);
 
 die('Thank you for your email');
 }
